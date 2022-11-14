ec2994ed7b8108564c54f316b37f3b0edfab16db
Fabio <92687822+FabioBnt@users.noreply.github.com>
2022-11-14 12:26:21 +0100

    TO finish
    Co-authored-by: Ismail <IsmailAlr@users.noreply.github.com>

--- modele/Connexion.php
+++ modele/Connexion.php
@@ -1,5 +1,4 @@
 <?php
-include 'Role.php';
 class Connexion {
   private $role;
   private $identifiant;
@@ -18,6 +17,36 @@ class Connexion {
       self::$instance = new Connexion();
     }
     return self::$instance;
+  }
+  function seConnecter() {
+    if (Role.ADMINISTRATEUR == role || Role.ARBITRE == role) {
+			if (comptes.get(role)[0].equals(identifiant) &&
+					comptes.get(role)[1].equals(Base64.getEncoder().encodeToString(mdp.getBytes()))) {
+				this.connexion = true;
+				this.role = role;
+			}
+		} else {
+      // id: BNF3924A mdp: iutinfo
+			Connection connx = JDBC.connecter();
+			try {
+				Statement st = connx.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+				ResultSet rs = st.executeQuery("select * from" + this.role + " where id = " + this.identifiant);
+				String result = null;
+				
+				while (rs.next()) {
+					result = rs.getString("mpd");
+				}
+				if (result != null) {
+					if (Base64.getEncoder().encodeToString(mdp.getBytes()).equals(result)) {
+						this.connexion = true;
+						this.role = role;
+					}
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
   }
   function getIdentifiant() {
     return $this->identifiant;
