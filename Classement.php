eab05264a753354003b3915f159c4bb4deab7080
FabioBnt <fabiobentaila@gmail.com>
2022-12-12 15:22:35 +0100

    Session connexion dans classement

---
+++ Classement.php
@@ -0,0 +1,72 @@
+﻿
+<?php 
+    include './modele/Connexion.php';
+    $connx = Connexion::getInstance();
+    if (isset($_GET['sedeconnecter'])) {
+        $connx->seDeconnecter();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>E-Sporter Manager</title>
+</head>
+<body class="classement">
+    <!--Menu de navigation-->
+    <header>
+        <div class="Menu">
+            <div class="menunav">
+            <nav class="navig">
+                <a href="index.php">Home</a>
+                <a href="ListeTournois.php">Liste des Tournois</a>
+                <a href="Classement.html">Classement</a>
+            </nav>
+            <div class="menucenter">
+                <img class="logo" src="./img/logo header.png">
+            </div>
+            </div>
+            <div class="menuright">
+                <div class="connecter">
+                    <a href="Connexion.php" id="connexion">Se Connecter</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="classementmain">
+            <h1>Classement Général</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th >Place</th>
+                            <th >Nom</th>
+                            <th >Nb de Points</th>
+                            <th >Plus d'informations</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Vitality</td>
+                            <td>2455</td>
+                            <td><a href="DetailsEquipe.html">+</a></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>MenwizzGaming</td>
+                            <td>2420</td>
+                            <td><a href="DetailsEquipe.html">+</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
