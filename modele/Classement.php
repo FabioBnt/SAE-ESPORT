714edf25b030c4a9fb1a2ab6a9d7580398330015
fujitoo <fabiobentaila@gmail.com>
2023-01-04 22:07:32 +0100

    WIP Classement (pas tout implémenté)

--- modele/Classement.php
+++ modele/Classement.php
@@ -1,5 +1,5 @@
 <?php
-
+include './modele/Database.php';
 class Classement
 {
     private $jeu;
@@ -10,6 +10,22 @@ class Classement
         $this->jeu = $jeu;
         $this->equipes = NULL;
         $this->classement = NULL;
+    }
+
+    public function getJeu()
+    {
+        return $this->jeu;
+    }
+
+    public function getEquipes()
+    {
+        return $this->equipes;
+    }
+
+    //Retourne le classement du tournoi pour le jeu passé en paramètre
+    public function returnClassement($jeu){
+        $db = Database::getInstance();
+        $data = $db->select('SELECT * FROM ');
     }
     
     public static function getInstance($jeu)
